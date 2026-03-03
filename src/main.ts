f773f1ec0ea90eee83460899178aa70df8942c64
Alexander Smirnov <smirnov.dev.frontend@gmail.com>
2026-03-03 22:07:34 +0300

    Исправлен main.ts

--- src/main.ts
+++ src/main.ts
@@ -7,39 +7,38 @@ import { API_URL } from './utils/constants';
 import { LarekApiClient } from './services/larek-api-client';
 import { ProductModel } from './models/product-model';
 import { CartModel } from './models/cart-model';
+import { OrderModel } from './models/order-model';
 
 import { Modal } from './views/modal';
 import { ProductListView } from './views/product-list';
-import { CatalogPresenter } from './presenters/catalog-presenter';
-
 import { HeaderBasketView } from './views/header-basket';
-import { CartPresenter } from './presenters/cart-presenter';
 
-import { OrderModel } from './models/order-model';
+import { CatalogPresenter } from './presenters/catalog-presenter';
+import { CartPresenter } from './presenters/cart-presenter';
 import { OrderPresenter } from './presenters/order-presenter';
+
+function mustGetElement<T extends HTMLElement>(selector: string): T {
+   const el = document.querySelector(selector);
+   if (!el) throw new Error(`Element not found: ${selector}`);
+   return el as T;
+}
 
 const events = new EventEmitter();
 const api = new Api(API_URL);
 const apiClient = new LarekApiClient(api);
 
-const gallery = document.querySelector('.gallery') as HTMLElement;
-const modalRoot = document.querySelector('#modal-container') as HTMLElement;
+const gallery = mustGetElement<HTMLElement>('.gallery');
+const modalRoot = mustGetElement<HTMLElement>('#modal-container');
 const pageRoot = document.body;
 
 const productModel = new ProductModel(apiClient);
 const cartModel = new CartModel();
+const orderModel = new OrderModel(apiClient);
 
 const productListView = new ProductListView(gallery);
 const modal = new Modal(modalRoot, events);
-
 const headerBasket = new HeaderBasketView(pageRoot, events);
 
-const cartPresenter = new CartPresenter(cartModel, productModel, headerBasket, modal, events);
-cartPresenter.init();
-
-const catalog = new CatalogPresenter(productModel, cartModel, productListView, modal, events);
-catalog.init();
-
-const orderModel = new OrderModel(apiClient);
-const orderPresenter = new OrderPresenter(orderModel, cartModel, modal, events);
-orderPresenter.init();
+new CartPresenter(cartModel, productModel, headerBasket, modal, events).init();
+new CatalogPresenter(productModel, cartModel, productListView, modal, events).init();
+new OrderPresenter(orderModel, cartModel, modal, events).init();
